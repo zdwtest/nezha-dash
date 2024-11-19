dab5ccc82c1cd1b1e63ff8173c5459647f23433e
ZHANG DAWEI <729913310@qq.com>
2024-11-20 00:40:25 +0800

    Update header.tsx
    更改网页标题

--- app/(main)/header.tsx
+++ app/(main)/header.tsx
@@ -39,7 +39,7 @@ function Header() {
               className="relative !m-0 border-2 border-transparent h-6 w-6 object-cover object-top !p-0"
             />
           </div>
-          {customTitle ? customTitle : "NezhaDash"}
+          {customTitle ? customTitle : "KaliJerry's Server"}
           <Separator
             orientation="vertical"
             className="mx-2 hidden h-4 w-[1px] md:block"
